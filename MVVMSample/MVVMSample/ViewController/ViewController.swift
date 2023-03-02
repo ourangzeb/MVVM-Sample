6113117a1760914e0ff8075ddcae870ff3c64eaa
Ourang-Zeb Khan <khanourangzaib@gmail.com>
2023-03-02 13:42:58 +0300

    tableview

--- MVVMSample/MVVMSample/ViewController/ViewController.swift
+++ MVVMSample/MVVMSample/ViewController/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension MainViewController:UITableViewDelegate {
+    
+}
+extension MainViewController:UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        
+        cell.titleLabel.text = "sdfsdf"
+        return cell
+    }
+    
+    numberof
+}
---
+++ MVVMSample/MVVMSample/Views/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  MVVMSample
+//
+//  Created by Khan on 02/03/2023.
+//
+
+import Foundation
+import UIKit
+
+
+class TableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var titleLabel: UIView!
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    
+}
