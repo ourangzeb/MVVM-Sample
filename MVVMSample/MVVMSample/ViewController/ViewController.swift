ab2774b0a77a1fe83c747b31c992e51e54dc6c82
Ourang-Zeb Khan <khanourangzaib@gmail.com>
2023-03-02 13:03:21 +0300

    code

---
+++ MVVMSample/MVVMSample/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMSample
+//
+//  Created by Khan on 02/03/2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
