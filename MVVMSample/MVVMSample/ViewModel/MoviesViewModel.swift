f88cd22bfbde4a6ecbae5e18ba3929e7ad60f752
Ourang-Zeb Khan <khanourangzaib@gmail.com>
2024-04-04 12:00:00 +0300

    coderefactoring

--- MVVMSample/MVVMSample/ViewModel/MoviesViewModel.swift
+++ MVVMSample/MVVMSample/ViewModel/MoviesViewModel.swift
@@ -66,6 +66,11 @@ class MovieViewModel : ObservableObject {
     
     
     
+    
+    
+    
+    
+    
     func fetchData(name : String) async throws {
         self.movies =  try! await movieUsecasetype.fetchData(with: name)
     }
